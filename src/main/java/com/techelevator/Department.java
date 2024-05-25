ce9ca7d75924cf421ca9204eb2857ce5ea8cd205
Eric Jacob <Explorer121@gmail.com[D[D[D[D[D[D[PJacob9[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[Cexplorer121@gmail.com>
2024-05-25 10:14:52 -0400

    Step One complete

---
+++ src/main/java/com/techelevator/Department.java
@@ -0,0 +1,29 @@
+package com.techelevator;
+
+public class Department {
+
+    private String name;
+
+    private int departmentId;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public Department(String name, int departmentId) {
+        this.name = name;
+        this.departmentId = departmentId;
+    }
+}
